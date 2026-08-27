889ee8ce96f2a6697d28cd66bee348a1a1648720
Lev-Dobrianskyi <lionnametu@gmail.com>
2026-08-27 17:31:07 +0300

    Add project files.

---
+++ urok 5/Controllers/TimeManagerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using urok_5.Models;
+
+namespace urok_5.Controllers
+{
+    public class TimeManagerController : Controller
+    {
+        public IActionResult TimeManager()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TimeManager(TimeManager obj)
+        {
+            return Content("Task: " + (String.IsNullOrEmpty(obj.Task) ? "Not provided" : obj.Task) + ", Time limit: " + (String.IsNullOrEmpty(obj.Time) ? "Not provided" : obj.Time));
+        }
+        public IActionResult ShowInfo(TimeManager obj)
+        {
+            return Content("Task: " + (String.IsNullOrEmpty(obj.Task) ? "Not provided" : obj.Task) + ", Time limit: " + (String.IsNullOrEmpty(obj.Time) ? "Not provided" : obj.Time));
+        }
+    }
+}
---
+++ urok 5/Models/TimeManager.cs
@@ -0,0 +1,9 @@
+﻿namespace urok_5.Models
+{
+    public class TimeManager
+    {
+        public int Id { get; set; }
+        public string Task { get; set; }
+        public string Time { get; set; }
+    }
+}
